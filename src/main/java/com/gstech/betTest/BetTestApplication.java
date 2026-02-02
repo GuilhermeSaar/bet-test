7b9ef67aef99dbd2d5cd0df57a7ffc8cfc88cfd5
Guilherme <guilhermesaar02@gmail.com>
2026-02-02 10:51:10 -0300

    start project

---
+++ src/main/java/com/gstech/betTest/BetTestApplication.java
@@ -0,0 +1,13 @@
+package com.gstech.betTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gstech/betTest/BetTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gstech.betTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
